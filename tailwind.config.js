3af74affcb5cc37a801bf471da48051b5ba59af9
terrnit <terrnit.megaman@gmail.com>
2022-11-25 16:09:18 +0500

    init and deploy to vercel

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html", "./node_modules/flowbite/**/*.js"],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          "50": "#eff6ff",
+          "100": "#dbeafe",
+          "200": "#bfdbfe",
+        }
+      }
+    },
+  },
+  plugins: [
+    require('flowbite/plugin')
+  ],
+}
